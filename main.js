bc3f461d6999d712fbefff95d8ca2b2a9d868d97
Steven Lim <128828904+nvliteesm@users.noreply.github.com>
2023-12-16 14:35:22 +0700

    Update main.js

--- main.js
+++ main.js
@@ -225,7 +225,7 @@ const animationTimeline = () => {
         }, {
             scale: 1,
             rotationY: 0,
-            color: "#ff69b4",
+            color: "#A020F0",
             ease: Expo.easeOut,
         },
         0.1,
